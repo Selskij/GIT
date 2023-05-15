8599606d7c1c768109f907f0977150de6b36f535
Selskij <selskij.v@edu.spbstu.ru>
2023-05-15 18:55:00 +0300

    changes in new

--- new.py
+++ new.py
@@ -11,3 +11,10 @@ if __name__ == '__main__':
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+    # Press the green button in the gutter to run the script.
+    if __name__ == '__main__':
+        print_hi('PyCharm')
+        # Press the green button in the gutter to run the script.
+        if __name__ == '__main__':
+            print_hi('PyCharm')
