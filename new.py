78c886d6cf5180e91ecb4357e2f1067d81ca006a
Selskij <selskij.v@edu.spbstu.ru>
2023-05-15 18:53:51 +0300

    changes in branch

--- new.py
+++ new.py
@@ -1,2 +1,13 @@
 print("Hello World")
 #comment
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
